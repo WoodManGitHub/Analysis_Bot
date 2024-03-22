3d103f6c6c6f28a9376811a73ca43625eafe739f
jimchen5209 <jimchen5209@gmail.com>
2024-03-22 19:31:35 +0800

    style: switch to eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+    'root': true,
+    'env': {
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4,
+            {
+                'SwitchCase': 1
+            }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'eqeqeq': 'error',
+        'no-unused-vars': 'warn',
+        'no-trailing-spaces': 'error',
+        'camelcase': 'warn',
+        'no-var': 'error',
+        'no-useless-return': 'error',
+        'no-else-return': 'error',
+        'no-empty': 'error',
+        'space-before-function-paren': 'error',
+        'space-in-parens': 'error',
+        'space-before-blocks': 'error',
+        'comma-dangle': 'error',
+        'no-duplicate-imports': 'error'
+    }
+};
